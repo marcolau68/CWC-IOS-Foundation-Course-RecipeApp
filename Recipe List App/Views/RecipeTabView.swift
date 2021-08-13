cf4079c307cabd5ba73355e7ae11e4cf1f472613
Marco <marcolau68@gmail.com>
2021-08-13 16:37:45 +0100

    Add RecipeTabView as starting screen

---
+++ Recipe List App/Views/RecipeTabView.swift
@@ -0,0 +1,32 @@
+//
+//  RecipeTabView.swift
+//  Recipe List App
+//
+//  Created by Marco Lau on 13/08/2021.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        TabView {
+            Text("Featured View")
+                .tabItem {
+                    Image(systemName: "star.fill")
+                    Text("Featured")
+                }
+            
+            RecipeListView()
+                .tabItem {
+                    Image(systemName: "list.bullet")
+                    Text("List")
+                }
+        }
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
