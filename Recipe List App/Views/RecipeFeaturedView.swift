859877e734eb68f7be2a35c7a841b85933d1deda
Marco <marcolau68@gmail.com>
2021-08-17 15:50:22 +0800

    - construct most of FeaturedView - tab card view for recipe

--- Recipe List App/Views/RecipeFeaturedView.swift
+++ Recipe List App/Views/RecipeFeaturedView.swift
@@ -12,12 +12,51 @@ struct RecipeFeaturedView: View {
     @EnvironmentObject var model: RecipeModel
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+        VStack(alignment: .leading, spacing: 0){
+            Text("Featured Recipes")
+                .padding(.leading)
+                .padding(.top, 30)
+                .font(.largeTitle)
+            
+            GeometryReader { geo in
+                TabView {
+                    ForEach(0..<model.recipeList.count) { index in
+                        if model.recipeList[index].featured {
+                            ZStack {
+                                Rectangle()
+                                    .foregroundColor(.white)
+                                    
+                                
+                                VStack(spacing: 0) {
+                                    Image(model.recipeList[index].image)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .clipped()
+                                    
+                                    Text(model.recipeList[index].name)
+                                        .padding(5)
+                                    
+                                    
+                                }
+                            }
+                            .frame(width: geo.size.width*5/6, height: geo.size.height*6/7)
+                            .cornerRadius(30)
+                            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                        }
+                    }
+                }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            }
+        }
     }
 }
 
 struct RecipeFeaturedView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeFeaturedView()
+            .environmentObject(RecipeModel())
     }
 }
